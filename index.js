d93fe2d81b5078c84600a5749da033a763e5a94f
caogiabao <caogiabao0909@gmail.com>
2025-03-26 07:19:22 +0700

    10. Kết nối project 1 với cơ sở dữ liệu

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
+require('dotenv').config()
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE);
+
+const Tour = mongoose.model('Tour', {
+  name: String,
+  vehicle: String
+});
+
 
 // Thiết lập views
 app.set('views', path.join(__dirname, "views"));
@@ -16,12 +25,22 @@ app.get('/', (req, res) => {
   })
 })
 
-app.get('/tour', (req, res) => {
+app.get('/tours', async (req, res) => {
+  const tourList = await Tour.find({});
+
+  console.log(tourList);
+
   res.render("client/pages/tour-list", {
-    pageTitle: "Danh sách tour"
+    pageTitle: "Danh sách tour",
+    tourList: tourList
   })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Website đang chạy trên cổng ${port}`)
 })
+
+
+// caogiabao0909 / tL6km1BIxEIsLMiE
+
+// mongodb+srv://caogiabao0909:tL6km1BIxEIsLMiE@cluster0.kljg2.mongodb.net/tour-management
