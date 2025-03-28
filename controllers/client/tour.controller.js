930d1a2d868a8eb33af674b82fe450119d6f4226
caogiabao <caogiabao0909@gmail.com>
2025-03-28 09:33:07 +0700

    2.1 Tạo trang chi tiết tour

--- controllers/client/tour.controller.js
+++ controllers/client/tour.controller.js
@@ -1,6 +1,5 @@
 const Tour = require("../../models/tour.model")
 
-
 module.exports.list = async (req, res) => {
   const tourList = await Tour.find({});
 
@@ -9,5 +8,11 @@ module.exports.list = async (req, res) => {
   res.render("client/pages/tour-list", {
     pageTitle: "Danh sách tour",
     tourList: tourList
+  })
+}
+
+module.exports.detail = async (req, res) => {
+  res.render("client/pages/tour-detail", {
+    pageTitle: "Chi tiết tour",
   })
 }
