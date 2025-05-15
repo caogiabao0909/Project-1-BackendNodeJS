35bc14dd5811ce30676bab60184b2b7bced44b66
caogiabao <caogiabao0909@gmail.com>
2025-05-15 19:35:36 +0700

    Add create tour & view tour list

--- controllers/admin/tour.controller.js
+++ controllers/admin/tour.controller.js
@@ -1,14 +1,98 @@
+const moment = require("moment");
+const Category = require("../../models/category.model")
+const City = require("../../models/city.model")
+const Tour = require("../../models/tour.model")
+const AccountAdmin = require("../../models/account-admin.model")
+
+const categoryHelper = require("../../helpers/category.helper")
+
 module.exports.list = async (req, res) => {
+  const find = {
+    deleted: false
+  }
+
+  const tourList = await Tour
+    .find(find)
+    .sort({
+      position: "desc"
+    })
+
+  for (const item of tourList) {
+    if (item.createdBy) {
+      const infoAccountCreated = await AccountAdmin.findOne({
+        _id: item.createdBy
+      })
+      item.createdByFullName = infoAccountCreated.fullName;
+    }
+
+    if (item.updatedBy) {
+      const infoAccountUpdated = await AccountAdmin.findOne({
+        _id: item.updatedBy
+      })
+      item.updatedByFullName = infoAccountUpdated.fullName;
+    }
+
+    item.createdAtFormat = moment(item.createdAt).format("HH:mm - DD/MM/YY")
+    item.updatedAtFormat = moment(item.updatedAt).format("HH:mm - DD/MM/YY")
+  }
+
   res.render("admin/pages/tour-list", {
     pageTitle: "Quản lý tour",
+    tourList: tourList
   })
 }
 
 module.exports.create = async (req, res) => {
+  const categoryList = await Category.find({
+    deleted: false
+  })
+
+  const categoryTree = categoryHelper.buildCategoryTree(categoryList)
+
+  const cityList = await City.find({})
+
   res.render("admin/pages/tour-create", {
     pageTitle: "Tạo tour",
+    categoryList: categoryTree,
+    cityList: cityList
   })
 }
+
+module.exports.createPost = async (req, res) => {
+  if (req.body.position) {
+    req.body.position = parseInt(req.body.position)
+  } else {
+    const totalRecord = await Tour.countDocuments({});
+    req.body.position = totalRecord + 1;
+  }
+
+  req.body.createdBy = req.account.id;
+  req.body.updatedBy = req.account.id;
+  req.body.avatar = req.file ? req.file.path : "";
+
+  req.body.priceAdult = req.body.priceAdult ? parseInt(req.body.priceAdult) : 0;
+  req.body.priceChildren = req.body.priceChildren ? parseInt(req.body.priceChildren) : 0;
+  req.body.priceBaby = req.body.priceBaby ? parseInt(req.body.priceBaby) : 0;
+  req.body.priceNewAdult = req.body.priceNewAdult ? parseInt(req.body.priceNewAdult) : req.body.priceAdult;
+  req.body.priceNewChildren = req.body.priceNewChildren ? parseInt(req.body.priceNewChildren) : req.body.priceChildren;
+  req.body.priceNewBaby = req.body.priceNewBaby ? parseInt(req.body.priceNewBaby) : req.body.priceBaby;
+  req.body.stockAdult = req.body.stockAdult ? parseInt(req.body.stockAdult) : 0;
+  req.body.stockChildren = req.body.stockChildren ? parseInt(req.body.stockChildren) : 0;
+  req.body.stockBaby = req.body.stockBaby ? parseInt(req.body.stockBaby) : 0;
+  req.body.locations = req.body.locations ? JSON.parse(req.body.locations) : [];
+  req.body.departureDate = req.body.departureDate ? new Date(req.body.departureDate) : null;
+  req.body.schedules = req.body.schedules ? JSON.parse(req.body.schedules) : [];
+
+  const newRocord = new Tour(req.body);
+  await newRocord.save();
+
+  req.flash("success", "Tạo tour thành công!")
+
+  res.json({
+    code: "success"
+  })
+}
+
 
 module.exports.trash = async (req, res) => {
   res.render("admin/pages/tour-trash", {
--- routes/admin/tour.route.js
+++ routes/admin/tour.route.js
@@ -1,10 +1,24 @@
 const router = require("express").Router();
+const multer = require('multer')
+
+const cloudinaryHelper = require("../../helpers/cloudinary.helper");
 
 const tourController = require("../../controllers/admin/tour.controller");
+
+const tourValidate = require("../../validates/admin/tour.validate")
+
+const upload = multer({ storage: cloudinaryHelper.storage })
 
 router.get('/list', tourController.list);
 
 router.get('/create', tourController.create);
+
+router.post(
+  '/create',
+  upload.single('avatar'),
+  tourValidate.createPost,
+  tourController.createPost
+);
 
 router.get('/trash', tourController.trash);
 
