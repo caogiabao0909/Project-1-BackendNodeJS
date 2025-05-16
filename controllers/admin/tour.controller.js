1cdc6ad0633a1e807f5336ab826e5af6c4a18f3a
caogiabao <caogiabao0909@gmail.com>
2025-05-16 10:58:46 +0700

    Add change multi in tour trash

--- controllers/admin/tour.controller.js
+++ controllers/admin/tour.controller.js
@@ -281,3 +281,35 @@ module.exports.deleteDestroyPatch = async (req, res) => {
     })
   }
 }
+
+module.exports.trashChangeMultiPatch = async (req, res) => {
+  try {
+    const { option, ids } = req.body;
+
+    switch (option) {
+      case "undo":
+        await Tour.updateMany({
+          _id: { $in: ids }
+        }, {
+          deleted: false
+        })
+        req.flash("success", "Khôi phục thành công!");
+        break;
+      case "delete-destroy":
+        await Tour.deleteMany({
+          _id: { $in: ids }
+        })
+        req.flash("success", "Xóa vĩnh viễn thành công!");
+        break;
+    }
+
+    res.json({
+      code: "success"
+    })
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Id không tồn tại trong hệ thống!"
+    })
+  }
+}
