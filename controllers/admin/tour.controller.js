a05207a9d0ee3defa4db3bb3713353a5daf3f6cf
caogiabao <caogiabao0909@gmail.com>
2025-05-16 10:39:30 +0700

    Add destroy tour in trash

--- controllers/admin/tour.controller.js
+++ controllers/admin/tour.controller.js
@@ -260,3 +260,24 @@ module.exports.undoPatch = async (req, res) => {
     })
   }
 }
+
+module.exports.deleteDestroyPatch = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Tour.deleteOne({
+      _id: id
+    })
+
+    req.flash("success", "Đã xóa vĩnh viễn tour thành công!")
+
+    res.json({
+      code: "success"
+    })
+  } catch (error) {
+    res.json({
+      code: "error",
+      message: "Id không hợp lệ!"
+    })
+  }
+}
