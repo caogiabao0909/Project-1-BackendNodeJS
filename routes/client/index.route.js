c2e93f63b3dd6e9935005e39d6233cf27de1d9fa
caogiabao <caogiabao0909@gmail.com>
2025-06-08 11:14:14 +0700

    add display data on header

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,8 +4,10 @@ const tourRoutes = require("./tour.route");
 const cartRoutes = require("./cart.route");
 
 const settingMiddleware = require("../../middlewares/client/setting.middleware.js")
+const categoryMiddleware = require("../../middlewares/client/category.middleware.js")
 
 router.use(settingMiddleware.websiteInfo);
+router.use(categoryMiddleware.list);
 
 router.use('/', homeRoutes);
 router.use('/tours', tourRoutes);
