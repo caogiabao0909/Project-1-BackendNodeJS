fdf589d4147768117d1d009b195afe8e06885ef2
caogiabao <caogiabao0909@gmail.com>
2025-08-03 22:31:48 +0700

    add display data on section 2 (home page)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,25 @@
-module.exports.home = (req, res) => {
+const Tour = require("../../models/tour.model")
+const moment = require("moment")
+
+module.exports.home = async (req, res) => {
+  // Section 2
+  const tourListSection2 = await Tour
+    .find({
+      deleted: false
+    })
+    .sort({
+      position: "desc"
+    })
+    .limit(6)
+
+  console.log(tourListSection2)
+  for (const item of tourListSection2) {
+    item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+  }
+  // End section 2
+
   res.render("client/pages/home", {
-    pageTitle: "Trang chủ"
+    pageTitle: "Trang chủ",
+    tourListSection2: tourListSection2
   })
 }
