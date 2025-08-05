7ee7302e9f71b35f8c4527af8f771823ed0e320e
caogiabao <caogiabao0909@gmail.com>
2025-08-05 10:55:25 +0700

    add display data on section 4 (home page)

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,5 @@
+const categoryHelper = require("../../helpers/category.helper")
+
 const Tour = require("../../models/tour.model")
 const moment = require("moment")
 
@@ -12,14 +14,34 @@ module.exports.home = async (req, res) => {
     })
     .limit(6)
 
-  console.log(tourListSection2)
   for (const item of tourListSection2) {
     item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
   }
   // End section 2
 
+  // Section 4: Tour trong nước
+  const categoryIdSection4 = "67ff2b2d818e0f155b2af8fe"
+  const listCategoryId = await categoryHelper.getAllSubcategoryIds(categoryIdSection4)
+
+  const tourListSection4 = await Tour
+    .find({
+      category: { $in: listCategoryId },
+      deleted: false,
+      status: "active"
+    })
+    .sort({
+      position: "desc"
+    })
+    .limit(8)
+
+  for (const item of tourListSection4) {
+    item.departureDateFormat = moment(item.departureDate).format("DD/MM/YYYY");
+  }
+  // Hết Section 4: Tour trong nước
+
   res.render("client/pages/home", {
     pageTitle: "Trang chủ",
-    tourListSection2: tourListSection2
+    tourListSection2: tourListSection2,
+    tourListSection4: tourListSection4
   })
 }
--- helpers/category.helper.js
+++ helpers/category.helper.js
@@ -1,3 +1,6 @@
+const Category = require("../models/category.model")
+
+// Lấy cây danh mục
 const buildCategoryTree = (categories, parentId = "") => {
   const tree = [];
 
@@ -18,3 +21,32 @@ const buildCategoryTree = (categories, parentId = "") => {
 }
 
 module.exports.buildCategoryTree = buildCategoryTree;
+// Hết lấy cây danh mục
+
+// Lấy tất cả id của danh mục cha và con
+module.exports.getAllSubcategoryIds = async (parentId) => {
+  const result = [parentId];
+
+  // Hàm tìm các danh mục con
+  const findChildren = async (currentId) => {
+    const children = await Category
+      .find({
+        parent: currentId,
+        deleted: false,
+        status: "active"
+      })
+      .sort({
+        position: "desc"
+      })
+
+    for (const child of children) {
+      result.push(child.id);
+      await findChildren(child.id);
+    }
+  }
+
+  await findChildren(parentId);
+
+  return result;
+}
+// Hết Lấy tất cả id của danh mục cha và con
