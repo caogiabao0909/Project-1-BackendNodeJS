48677023de9a7bf318e431d21ff5df1b004a58ad
caogiabao <caogiabao0909@gmail.com>
2025-08-18 21:43:38 +0700

    add breadcumb (tour-detail page)

--- controllers/client/tour.controller.js
+++ controllers/client/tour.controller.js
@@ -1,7 +1,72 @@
+const Category = require("../../models/category.model");
 const Tour = require("../../models/tour.model")
 
 module.exports.detail = async (req, res) => {
-  res.render("client/pages/tour-detail", {
-    pageTitle: "Chi tiết tour",
-  })
+  try {
+    const slug = req.params.slug;
+
+    const tourDetail = await Tour
+      .findOne({
+        slug: slug,
+        deleted: false,
+        status: "active",
+      })
+
+    if (!tourDetail) {
+      return res.redirect("/")
+    }
+
+    const breadcrumb = {
+      image: tourDetail.avatar,
+      title: tourDetail.name,
+      list: [
+        {
+          link: "/",
+          title: "Trang chủ"
+        }
+      ]
+    };
+
+    const category = await Category.findOne({
+      _id: tourDetail.category,
+      deleted: false,
+      status: "active"
+    })
+
+    if (category) {
+      if (category.parent) {
+        const parentCategory = await Category.findOne({
+          _id: category.parent,
+          deleted: false,
+          status: "active"
+        })
+
+        if (parentCategory) {
+          breadcrumb.list.push({
+            link: `/category/${parentCategory.slug}`,
+            title: parentCategory.name
+          })
+        }
+      }
+
+      breadcrumb.list.push({
+        link: `/category/${category.slug}`,
+        title: category.name
+      })
+    }
+
+    breadcrumb.list.push({
+      link: `/tour/detail/${slug}`,
+      title: tourDetail.name
+    })
+
+    res.render("client/pages/tour-detail", {
+      pageTitle: "Chi tiết tour",
+      breadcrumb
+    })
+
+  } catch (error) {
+    console.log(error);
+    res.redirect("/")
+  }
 }
