c88f309f993612095a8871c282917af7a59b9f67
caogiabao <caogiabao0909@gmail.com>
2025-09-10 15:33:12 +0700

    add tour into cart feature

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -486,3 +486,76 @@ if (formSearch) {
   })
 }
 // End Form Search
+
+// Box Tour Detail
+const boxTourDetail = document.querySelector(".box-tour-detail")
+if (boxTourDetail) {
+  const inputStockAdult = document.querySelector("[input-stock-adult]")
+  const inputStockChildren = document.querySelector("[input-stock-children]")
+  const inputStockBaby = document.querySelector("[input-stock-baby]")
+
+  const drawBoxDetail = () => {
+    const quantityAdult = parseInt(inputStockAdult.value)
+    const quantityChilren = parseInt(inputStockChildren.value)
+    const quantityBaby = parseInt(inputStockBaby.value)
+
+    const stockAdult = document.querySelector("[stock-adult]")
+    const stockChildren = document.querySelector("[stock-children]")
+    const stockBaby = document.querySelector("[stock-baby]")
+
+    stockAdult.innerHTML = quantityAdult;
+    stockChildren.innerHTML = quantityChilren;
+    stockBaby.innerHTML = quantityBaby;
+
+    const priceAdult = parseInt(inputStockAdult.getAttribute("price"))
+    const priceChildren = parseInt(inputStockChildren.getAttribute("price"))
+    const priceBaby = parseInt(inputStockBaby.getAttribute("price"))
+    const totalPrice = (priceAdult * quantityAdult) + (priceChildren * quantityChilren) + (priceBaby * quantityBaby)
+
+    const elementTotalPrice = document.querySelector("[total-price]")
+    elementTotalPrice.innerHTML = totalPrice.toLocaleString("vi-VN")
+  }
+
+  inputStockAdult.addEventListener("change", drawBoxDetail)
+  inputStockChildren.addEventListener("change", drawBoxDetail)
+  inputStockBaby.addEventListener("change", drawBoxDetail)
+
+  const buttonAddToCart = boxTourDetail.querySelector(".inner-button-add-cart")
+  buttonAddToCart.addEventListener("click", () => {
+    const tourId = buttonAddToCart.getAttribute("tour-id")
+    const quantityAdult = parseInt(inputStockAdult.value)
+    const quantityChilren = parseInt(inputStockChildren.value)
+    const quantityBaby = parseInt(inputStockBaby.value)
+    const locationFrom = boxTourDetail.querySelector("[location-from]").value;
+
+    if (quantityAdult > 0 || quantityChilren > 0 || quantityBaby > 0) {
+      const cartItem = {
+        tourId,
+        quantityAdult,
+        quantityChilren,
+        quantityBaby,
+        locationFrom,
+      }
+
+      const cart = JSON.parse(localStorage.getItem("cart"))
+      const indexItemExist = cart.findIndex(item => item.tourId == tourId)
+      if (indexItemExist != -1) {
+        cart[indexItemExist] = cartItem;
+      } else {
+        cart.push(cartItem);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(cart));
+      window.location.href = "/cart"
+    }
+  })
+}
+// End Box Tour Detail
+
+// Inittial Cart
+const cart = localStorage.getItem("cart");
+if (!cart) {
+  localStorage.setItem("cart", JSON.stringify([]))
+}
+// End Inittial Cart
+
