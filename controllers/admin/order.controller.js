11cd354725a18092c20f9df06427268e036ad3d6
caogiabao <caogiabao0909@gmail.com>
2025-09-25 10:27:21 +0700

    add order management page

--- controllers/admin/order.controller.js
+++ controllers/admin/order.controller.js
@@ -1,6 +1,32 @@
+const { paymentMethod, paymentStatus, orderStatus } = require("../../config/variable")
+const moment = require("moment")
+const Order = require("../../models/order.model")
+
 module.exports.list = async (req, res) => {
+  const find = {
+    deleted: false
+  }
+
+  const orderList = await Order
+    .find(find)
+    .sort({
+      createAt: "desc"
+    })
+
+
+  for (const orderDetail of orderList) {
+    orderDetail.paymentMethodName = paymentMethod.find(item => item.value === orderDetail.paymentMethod).label;
+    orderDetail.paymentStatusName = paymentStatus.find(item => item.value === orderDetail.paymentStatus).label;
+    orderDetail.orderStatusName = orderStatus.find(item => item.value === orderDetail.status).label;
+    orderDetail.createdAtTime = moment(orderDetail.createdAt).format("HH:mm")
+    orderDetail.createdAtDate = moment(orderDetail.createdAt).format("DD/MM/YYYY")
+  }
+  console.log(orderList)
+
+
   res.render("admin/pages/order-list", {
     pageTitle: "Quản lý đơn hàng",
+    orderList
   })
 }
 
