282731002dccb39bdd81154c5ce86aeec4a1239d
caogiabao <caogiabao0909@gmail.com>
2025-10-05 19:49:27 +0700

    add display over view metrics section on dashboard page

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,30 @@
+const AccountAdmin = require("../../models/account-admin.model")
+const Order = require("../../models/order.model")
+
 module.exports.dashboard = async (req, res) => {
+  const overview = {
+    totalAdmin: 0,
+    totalOrder: 0,
+    revenue: 0,
+  }
+
+  overview.totalAdmin = await AccountAdmin.countDocuments({
+    deleted: false
+  })
+
+  const orderList = await Order.find({
+    deleted: false
+  })
+
+  overview.totalOrder = orderList.length;
+
+  overview.revenue = orderList.reduce((sum, item) => {
+    return (item.paymentStatus === "paid") ? sum + item.total : sum;
+  }, 0)
+
+
   res.render("admin/pages/dashboard", {
     pageTitle: "Tổng quan",
+    overview
   })
 }
